5726ea034cbbf58ba00d0b759cb2d3f98dbbed14
saikeerthisri-N221156 <n221156@rguktn.ac.in>
2026-01-05 16:59:11 +0000

    javascript to agri website

--- wtlab/script.js
+++ wtlab/script.js
@@ -30,8 +30,7 @@ arrowfun("keerthi");
 
 //arrow function for addition and subtraction
 const add=(a,b)=>{
-    console.log("addition:",a+b);
-    
+    console.log("addition:",a+b);   
 }
 add(10,20);
 
@@ -42,6 +41,9 @@ console.log("multiplication of 10 and 5 is: ",mult(10,5));
 
 
 
-
+//function expression
+function func(){
+    
+}
 
 
